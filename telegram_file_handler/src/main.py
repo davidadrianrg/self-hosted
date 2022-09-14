8bfe78151ac599e40bb00000b286d321a0a238c1
David Adrian Rodriguez Garcia <davidadrianrg@gmail.com>
2022-09-14 18:49:23 +0200

    Add extracting functionality

--- telegram_file_handler/src/main.py
+++ telegram_file_handler/src/main.py
@@ -2,12 +2,17 @@
 from telegram import Bot, ChatAction
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from os import getenv, path
+import os
 from time import sleep
+import shutil
+import zipfile
+import rarfile
 
-TOKEN = getenv('TELEGRAM_TOKEN')
-WATCH_FOLDER = getenv('WATCH_FOLDER')
-USER_ID = int(getenv('TELEGRAM_USER_ID'))
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+WATCH_FOLDER = os.getenv('WATCH_FOLDER')
+DOWNLOADS_FOLDER = os.getenv('DOWNLOADS_FOLDER')
+#FILMS_FOLDER = os.getenv('FILMS_FOLDER')
+USER_ID = int(os.getenv('TELEGRAM_USER_ID'))
 bot = Bot(token=TOKEN)
 
 
@@ -19,14 +24,14 @@ def torrent_downloader(update, context) :
 
     try:   
         message = update.message.reply_text("***Procesando...***")
-        #downloading file
-        file_id = update.message.document.file_id  #getting file id
+        # Downloading file
+        file_id = update.message.document.file_id  # Getting file id
         newFile = bot.get_file(file_id)
         
-        fileName = update.message.document.file_name   #getting filename
+        fileName = update.message.document.file_name   # Getting filename
         
-        #Downloading file directly in watch folder
-        newFile.download(custom_path=path.join(WATCH_FOLDER,fileName))
+        # Downloading file directly in watch folder
+        newFile.download(custom_path=os.path.join(WATCH_FOLDER,fileName))
         
         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
         message_id = message.message_id
@@ -36,10 +41,44 @@ def torrent_downloader(update, context) :
     except Exception as e:
         print(e)    
 
+def extract(update, context) -> None:
+    update.message.reply_text("Extrayendo ficheros de la carpeta de descargas...")
+    # Creating a list with all the files to be extracted
+    subfolders = [f.path for f in os.scandir(os.path.join(DOWNLOADS_FOLDER, "complete")) if f.is_dir()]
+
+    for sub in subfolders:
+        for f in os.listdir(sub):
+            src_file = os.path.join(sub, f)
+            extension = f.split(".")[-1]
+            if extension == "zip":
+                try:
+                    with zipfile.ZipFile(src_file) as z:
+                        for file in z.namelist():
+                            if file.endswith(".avi") or file.endswith(".mkv") or file.endswith(".mp4"):
+                                z.extract(file, path=os.path.join(DOWNLOADS_FOLDER, "extracted"))
+                        print(f"Extracted file {src_file}")
+                except:
+                    print("Invalid file, error extracting")
+            if extension == "rar":
+                try:
+                    with rarfile.RarFile(src_file) as z:
+                        for file in z.namelist():
+                            f_endswith = file.split(".")[-1]
+                            part_file = file.split(".")[-2]
+                            if part_file.contains("part") and part_file != "part01": 
+                                is_other_part = True
+                            else: is_other_part = False
+                            if f_endswith == "avi" or f_endswith == "mkv" or f_endswith == "mp4" and not is_other_part:
+                                z.extract(file, path=os.path.join(DOWNLOADS_FOLDER, "extracted"))
+                        print(f"Extracted file {src_file}")
+                except:
+                    print("Invalid file, error extracting")
+            
        
 
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler(CommandHandler('start', starter, Filters.user(user_id=USER_ID)))
+updater.dispatcher.add_handler(CommandHandler('extract', extract, Filters.user(user_id=USER_ID)))
 
 updater.dispatcher.add_handler(MessageHandler(Filters.chat_type.private, torrent_downloader))
 
