0e114e466b518156f168201b75be017294a1a802
David Adrián Rodríguez García <davidadrianrg@gmail.com>
2023-03-07 17:03:40 +0100

    change gaming network name

--- networking/docker-networking.sh
+++ networking/docker-networking.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Public network with proxy connection
 docker network create --driver=bridge public
+# Private network without internet connection
 docker network create --driver=bridge --internal private
-docker network create --driver=bridge gaming
+# Public network with tcp/udp open ports on host
+docker network create --driver=bridge on_tcp
